521f00414be022f38d43284552257357b4da43bc
Libor Gabaj <libor.gabaj@gmail.com>
2020-02-19 19:54:05 +0100

    Fixed installation setup.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 """Setup function for the package."""
 
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
   name='gbj_timer',
-  version='1.0.0',
+  version='1.0.1',
   description='Python package for module timer.',
   long_description='Module for managing timers.',
   classifiers=[
@@ -20,7 +20,7 @@ setup(
   author='Libor Gabaj',
   author_email='libor.gabaj@gmail.com',
   license='MIT',
-  packages=['gbj_timer'],
+  packages=find_namespace_packages(),
   install_requires=[],
   include_package_data=True,
   zip_safe=False
